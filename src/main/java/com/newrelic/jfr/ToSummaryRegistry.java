5f789a3e71337cd2d706cc86ebc951935bbb5516
jplumb <jplumb@newrelic.com>
2020-05-28 14:52:42 -0700

    reformat

--- src/main/java/com/newrelic/jfr/ToSummaryRegistry.java
+++ src/main/java/com/newrelic/jfr/ToSummaryRegistry.java
@@ -17,45 +17,42 @@ import static java.util.stream.Collectors.toUnmodifiableList;
 
 public class ToSummaryRegistry {
 
-    private final static List<EventToSummary> ALL_MAPPERS = List.of(
-            new G1GarbageCollectionSummarizer(),
-            new NetworkReadSummarizer(),
-            new NetworkWriteSummarizer(),
-            new ObjectAllocationInNewTLABSummarizer(),
-            new ObjectAllocationOutsideTLABSummarizer()
-    );
+  private static final List<EventToSummary> ALL_MAPPERS =
+      List.of(
+          new G1GarbageCollectionSummarizer(),
+          new NetworkReadSummarizer(),
+          new NetworkWriteSummarizer(),
+          new ObjectAllocationInNewTLABSummarizer(),
+          new ObjectAllocationOutsideTLABSummarizer());
 
-    private final List<EventToSummary> mappers;
+  private final List<EventToSummary> mappers;
 
-    private ToSummaryRegistry(List<EventToSummary> mappers) {
-        this.mappers = mappers;
-    }
+  private ToSummaryRegistry(List<EventToSummary> mappers) {
+    this.mappers = mappers;
+  }
 
-    public static ToSummaryRegistry createDefault() {
-        return new ToSummaryRegistry(ALL_MAPPERS);
-    }
+  public static ToSummaryRegistry createDefault() {
+    return new ToSummaryRegistry(ALL_MAPPERS);
+  }
 
-    public static ToSummaryRegistry create(Collection<String> eventNames) {
-        var filtered =
-                ALL_MAPPERS
-                        .stream()
-                        .filter(mapper -> eventNames.contains(mapper.getEventName()))
-                        .collect(toList());
-        return new ToSummaryRegistry(filtered);
-    }
+  public static ToSummaryRegistry create(Collection<String> eventNames) {
+    var filtered =
+        ALL_MAPPERS
+            .stream()
+            .filter(mapper -> eventNames.contains(mapper.getEventName()))
+            .collect(toList());
+    return new ToSummaryRegistry(filtered);
+  }
 
+  private static List<String> allEventNames() {
+    return ALL_MAPPERS.stream().map(EventToSummary::getEventName).collect(toUnmodifiableList());
+  }
 
-    private static List<String> allEventNames() {
-        return ALL_MAPPERS.stream().map(EventToSummary::getEventName).collect(toUnmodifiableList());
-    }
+  public Stream<EventToSummary> all() {
+    return mappers.stream();
+  }
 
-    public Stream<EventToSummary> all() {
-        return mappers.stream();
-    }
-
-    public Optional<EventToSummary> get(String eventName) {
-        return mappers.stream()
-        .filter(m -> m.getEventName().equals(eventName))
-        .findFirst();
-    }
+  public Optional<EventToSummary> get(String eventName) {
+    return mappers.stream().filter(m -> m.getEventName().equals(eventName)).findFirst();
+  }
 }
