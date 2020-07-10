aa9b958a4fbdf381da90f076226e855413182a62
jplumb <jplumb@newrelic.com>
2020-07-10 11:33:06 -0700

    only specify the URIs when present, otherwise just use SDK default

--- jfr-daemon/src/main/java/com/newrelic/jfr/daemon/TelemetryClientFactory.java
+++ jfr-daemon/src/main/java/com/newrelic/jfr/daemon/TelemetryClientFactory.java
@@ -1,9 +1,5 @@
 package com.newrelic.jfr.daemon;
 
-import static com.newrelic.jfr.daemon.EnvironmentVars.EVENTS_INGEST_URI;
-import static com.newrelic.jfr.daemon.EnvironmentVars.INSERT_API_KEY;
-import static com.newrelic.jfr.daemon.EnvironmentVars.METRICS_INGEST_URI;
-
 import com.newrelic.telemetry.EventBatchSenderFactory;
 import com.newrelic.telemetry.Java11HttpPoster;
 import com.newrelic.telemetry.MetricBatchSenderFactory;
@@ -11,40 +7,49 @@ import com.newrelic.telemetry.TelemetryClient;
 import com.newrelic.telemetry.events.EventBatchSender;
 import com.newrelic.telemetry.http.HttpPoster;
 import com.newrelic.telemetry.metrics.MetricBatchSender;
+
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.function.Supplier;
 
-/** Builds the instance of the TelemetryClient used to send data to New Relic ingest endpoints. */
+/**
+ * Builds the instance of the TelemetryClient used to send data to New Relic ingest endpoints.
+ */
 public class TelemetryClientFactory {
 
-  public TelemetryClient build(DaemonConfig config) throws MalformedURLException {
+    private final Supplier<HttpPoster> httpPosterCreator =
+            () -> new Java11HttpPoster(Duration.of(10, ChronoUnit.SECONDS));
 
-    Supplier<HttpPoster> httpPosterCreator =
-        () -> new Java11HttpPoster(Duration.of(10, ChronoUnit.SECONDS));
+    public TelemetryClient build(DaemonConfig config) throws MalformedURLException {
+        var metricBatchSender = buildMetricBatchSender(config);
+        var eventBatchSender = buildEventBatchSender(config);
+        return new TelemetryClient(metricBatchSender, null, eventBatchSender, null);
+    }
 
-    String userAgent = "JFR-Daemon/" + config.getDaemonVersion();
-
-    var metricBatchSender =
-        MetricBatchSender.create(
-            MetricBatchSenderFactory.fromHttpImplementation(httpPosterCreator)
+    private EventBatchSender buildEventBatchSender(DaemonConfig config) throws MalformedURLException {
+        var eventsConfig = EventBatchSenderFactory.fromHttpImplementation(httpPosterCreator)
                 .configureWith(config.getApiKey())
-                .endpointWithPath(config.getMetricsUri().toURL())
-                .secondaryUserAgent(userAgent)
                 .auditLoggingEnabled(true)
-                .build());
+                .secondaryUserAgent(makeUserAgent(config));
+        if (config.getEventsUri() != null) {
+            eventsConfig = eventsConfig.endpointWithPath(config.getEventsUri().toURL());
+        }
+        return EventBatchSender.create(eventsConfig.build());
+    }
 
-    var eventBatchSender =
-        EventBatchSender.create(
-            EventBatchSenderFactory.fromHttpImplementation(httpPosterCreator)
+    private MetricBatchSender buildMetricBatchSender(DaemonConfig config) throws MalformedURLException {
+        var metricConfig = MetricBatchSenderFactory.fromHttpImplementation(httpPosterCreator)
                 .configureWith(config.getApiKey())
-                .endpointWithPath(config.getEventsUri().toURL())
-                .auditLoggingEnabled(true)
-                .secondaryUserAgent(userAgent)
-                .build());
+                .secondaryUserAgent(makeUserAgent(config))
+                .auditLoggingEnabled(true);
+        if (config.getMetricsUri() != null) {
+            metricConfig = metricConfig.endpointWithPath(config.getMetricsUri().toURL());
+        }
+        return MetricBatchSender.create(metricConfig.build());
+    }
 
-    return new TelemetryClient(metricBatchSender, null, eventBatchSender, null);
-  }
+    private String makeUserAgent(DaemonConfig config) {
+        return "JFR-Daemon/" + config.getDaemonVersion();
+    }
 }
