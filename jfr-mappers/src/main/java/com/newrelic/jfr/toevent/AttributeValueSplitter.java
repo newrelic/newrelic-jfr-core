2fe5c2a9293b66a228d8e2403dcc63bee309512c
Xi Xia <xxia@newrelic.com>
2021-04-21 16:40:22 -0700

    check and split attribute value

---
+++ jfr-mappers/src/main/java/com/newrelic/jfr/toevent/AttributeValueSplitter.java
@@ -0,0 +1,24 @@
+package com.newrelic.jfr.toevent;
+
+import com.newrelic.telemetry.Attributes;
+
+public class AttributeValueSplitter {
+
+  static final int MAX_LENGTH = 4096; // Event API attribute size limit
+
+  void maybeSplit(Attributes attr, String key, String value) {
+    int extendedCount = 0;
+    while (value.length() > MAX_LENGTH) {
+      String firstBlock = value.substring(0, MAX_LENGTH);
+      attr.put(incrementKey(extendedCount, key), firstBlock);
+      value = value.substring(MAX_LENGTH);
+      extendedCount = extendedCount + 1;
+    }
+    attr.put(incrementKey(extendedCount, key), value);
+  }
+
+  String incrementKey(int extendedCount, String key) {
+    String KEY_EXTENDED = "Extended_";
+    return extendedCount == 0 ? key : key + KEY_EXTENDED + extendedCount;
+  }
+}
