d5c52ab37d91f98aae68e1b98aa1fa9d1a15a645
Andre Onuki <otanuki@gmail.com>
2021-07-29 11:18:21 -0400

    Preventing serialization and deserialization of stack traces

--- jfr-mappers/src/main/java/com/newrelic/jfr/profiler/ProfileSummarizer.java
+++ jfr-mappers/src/main/java/com/newrelic/jfr/profiler/ProfileSummarizer.java
@@ -2,20 +2,21 @@ package com.newrelic.jfr.profiler;
 
 import static com.newrelic.jfr.RecordedObjectValidators.hasField;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.newrelic.jfr.MethodSupport;
 import com.newrelic.jfr.ThreadNameNormalizer;
 import com.newrelic.jfr.profiler.FlamegraphMarshaller.StackFrame;
 import com.newrelic.telemetry.Attributes;
 import com.newrelic.telemetry.events.Event;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import jdk.jfr.consumer.RecordedEvent;
+import jdk.jfr.consumer.RecordedFrame;
 import jdk.jfr.consumer.RecordedStackTrace;
 import jdk.jfr.consumer.RecordedThread;
 
@@ -23,11 +24,9 @@ public class ProfileSummarizer implements EventToEventSummary {
   public static final String EVENT_NAME = "jdk.ExecutionSample";
   public static final String NATIVE_EVENT_NAME = "jdk.NativeMethodSample";
   public static final String SIMPLE_CLASS_NAME = ProfileSummarizer.class.getSimpleName();
-  public static final String THREAD_STATE = "thread.state";
   public static final String STATE = "state";
   public static final String THREAD_NAME = "thread.name";
   public static final String SAMPLED_THREAD = "sampledThread";
-  public static final String STACK_TRACE = "stackTrace";
   public static final String JFR_FLAMELEVEL = "JfrFlameLevel";
   public static final String FLAME_NAME = "flamelevel.name";
   public static final String FLAME_VALUE = "flamelevel.value";
@@ -73,20 +72,17 @@ public class ProfileSummarizer implements EventToEventSummary {
     }
     timestamp.updateAndGet(current -> Math.min(current, ev.getStartTime().toEpochMilli()));
 
-    Map<String, String> jfrStackTrace = new HashMap<>();
-
     String threadName = null;
     if (hasField(ev, SAMPLED_THREAD, SIMPLE_CLASS_NAME)) {
       RecordedThread sampledThread = ev.getThread(SAMPLED_THREAD);
       threadName = nameNormalizer.getNormalizedThreadName(sampledThread.getJavaName());
     }
-    jfrStackTrace.put(THREAD_NAME, threadName);
 
+    String threadState = null;
     if (hasField(ev, STATE, SIMPLE_CLASS_NAME)) {
-      jfrStackTrace.put(THREAD_STATE, ev.getString(STATE));
+      threadState = ev.getString(STATE);
     }
-    jfrStackTrace.put(STACK_TRACE, MethodSupport.serialize(ev.getStackTrace()));
-    JvmStackTraceEvent event = stackTraceToStackFrames(jfrStackTrace);
+    JvmStackTraceEvent event = stackTraceToStackFrames(threadName, threadState, ev.getStackTrace());
 
     stackTraceEventPerThread
         .computeIfAbsent(event.getThreadName(), k -> new ArrayList<>())
@@ -95,29 +91,19 @@ public class ProfileSummarizer implements EventToEventSummary {
 
   @Override
   public Stream<Event> summarize() {
-    Map<String, StackFrame> stackFrameByThread =
+    Stream<Event> eventStream =
         stackTraceEventPerThread
             .entrySet()
             .stream()
-            .collect(
-                Collectors.toMap(
-                    Map.Entry::getKey, event -> stackTraceToStackFrame(event.getValue())));
+            .map(entry -> stackTraceToEvent(entry.getKey(), entry.getValue()))
+            .flatMap(Collection::stream);
+    return eventStream;
+  }
 
-    Map<String, List<FlameLevel>> flameLevelsByThread =
-        stackFrameByThread
-            .entrySet()
-            .stream()
-            .collect(
-                Collectors.toMap(
-                    Map.Entry::getKey, stackframe -> flattener.flatten(stackframe.getValue())));
-
-    List<Event> events =
-        flameLevelsByThread
-            .entrySet()
-            .stream()
-            .flatMap(e -> flameLevelToEvent(e.getValue(), e.getKey()).stream())
-            .collect(Collectors.toList());
-    return events.stream();
+  private List<Event> stackTraceToEvent(String threadName, List<JvmStackTraceEvent> stackTraces) {
+    StackFrame stackFrame = stackTraceToStackFrame(stackTraces);
+    List<FlameLevel> flameLevels = flattener.flatten(stackFrame);
+    return flameLevelToEvent(flameLevels, threadName);
   }
 
   private List<Event> flameLevelToEvent(List<FlameLevel> flameLevels, String threadName) {
@@ -156,31 +142,18 @@ public class ProfileSummarizer implements EventToEventSummary {
     return entryBuilder.toString();
   }
 
-  private static JvmStackTraceEvent stackTraceToStackFrames(Map<String, String> jfrStackTrace) {
-    String name = jfrStackTrace.get(THREAD_NAME);
-    String state = jfrStackTrace.get(THREAD_STATE);
-    JsonElement jsonTree = JsonParser.parseString(jfrStackTrace.get(STACK_TRACE));
-    if (jsonTree.isJsonObject()) {
-      JsonObject json = jsonTree.getAsJsonObject();
-      JsonArray payload = json.getAsJsonArray("payload");
-      List<JvmStackTraceEvent.JvmStackFrame> out = new ArrayList<>();
+  private static JvmStackTraceEvent stackTraceToStackFrames(
+      String name, String state, RecordedStackTrace stackTrace) {
+    List<JvmStackTraceEvent.JvmStackFrame> out = new ArrayList<>();
 
-      for (JsonElement element : payload) {
-        if (element.isJsonObject()) {
-          JsonObject jFrame = element.getAsJsonObject();
-          String desc = jFrame.get("desc").getAsString();
-          int line = jFrame.get("line").getAsInt();
-          int bytecodeIndex = jFrame.get("bytecodeIndex").getAsInt();
-
-          out.add(new JvmStackTraceEvent.JvmStackFrame(desc, line, bytecodeIndex));
-        } else {
-          // log error
-        }
-      }
-      return new JvmStackTraceEvent(name, state, out);
-    } else {
-      return new JvmStackTraceEvent(name, state, Collections.emptyList());
+    for (RecordedFrame frame : stackTrace.getFrames()) {
+      String desc = MethodSupport.describeMethod(frame.getMethod());
+      int line = frame.getLineNumber();
+      int bytecodeIndex = frame.getBytecodeIndex();
+      out.add(new JvmStackTraceEvent.JvmStackFrame(desc, line, bytecodeIndex));
     }
+
+    return new JvmStackTraceEvent(name, state, out);
   }
 
   @Override
--- jfr-mappers/src/test/java/com/newrelic/jfr/profiler/ProfileSummarizerTest.java
+++ jfr-mappers/src/test/java/com/newrelic/jfr/profiler/ProfileSummarizerTest.java
@@ -6,16 +6,18 @@ import static com.newrelic.jfr.profiler.ProfileSummarizer.STATE;
 import static com.newrelic.jfr.profiler.ProfileSummarizer.THREAD_NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import com.newrelic.jfr.MethodSupport;
 import com.newrelic.jfr.RecordedObjectValidators;
 import com.newrelic.jfr.ThreadNameNormalizer;
 import com.newrelic.telemetry.events.Event;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import jdk.jfr.consumer.RecordedEvent;
+import jdk.jfr.consumer.RecordedFrame;
 import jdk.jfr.consumer.RecordedStackTrace;
 import jdk.jfr.consumer.RecordedThread;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,9 +30,6 @@ import org.mockito.MockitoAnnotations;
 
 class ProfileSummarizerTest {
 
-  private final String stackTrace =
-      "{\"type\":\"stacktrace\",\"language\":\"java\",\"version\":1,\"truncated\":false,\"payload\":[{\"desc\":\"java.net.PlainSocketImpl.socketAccept(Ljava/net/SocketImpl;)V\",\"line\":\"-1\",\"bytecodeIndex\":\"0\"},{\"desc\":\"java.net.AbstractPlainSocketImpl.accept(Ljava/net/SocketImpl;)V\",\"line\":\"458\",\"bytecodeIndex\":\"7\"},{\"desc\":\"java.net.ServerSocket.implAccept(Ljava/net/Socket;)V\",\"line\":\"551\",\"bytecodeIndex\":\"60\"},{\"desc\":\"java.net.ServerSocket.accept()Ljava/net/Socket;\",\"line\":\"519\",\"bytecodeIndex\":\"48\"},{\"desc\":\"sun.rmi.transport.tcp.TCPTransport$AcceptLoop.executeAcceptLoop()V\",\"line\":\"394\",\"bytecodeIndex\":\"42\"},{\"desc\":\"sun.rmi.transport.tcp.TCPTransport$AcceptLoop.run()V\",\"line\":\"366\",\"bytecodeIndex\":\"1\"},{\"desc\":\"java.lang.Thread.run()V\",\"line\":\"834\",\"bytecodeIndex\":\"11\"}]}";
-
   @Mock(answer = Answers.RETURNS_DEEP_STUBS)
   private RecordedEvent mockEvent;
 
@@ -42,7 +41,6 @@ class ProfileSummarizerTest {
   @Mock private RecordedThread mockThread;
   @Mock private RecordedThread mockThread2;
   @Mock private RecordedStackTrace mockStackTrace;
-  @Mock private RecordedStackTrace mockStackTrace2;
 
   @BeforeEach
   public void setup() {
@@ -56,8 +54,26 @@ class ProfileSummarizerTest {
     when(mockEvent2.getThread(SAMPLED_THREAD)).thenReturn(mockThread2);
     when(mockThread2.getJavaName()).thenReturn("thread-2");
     when(mockEvent2.getString(STATE)).thenReturn("running");
-    when(mockEvent2.getStackTrace()).thenReturn(mockStackTrace2);
+    when(mockEvent2.getStackTrace()).thenReturn(mockStackTrace);
     when(mockEvent2.getStartTime().toEpochMilli()).thenReturn(2L);
+
+    List<RecordedFrame> frames =
+        Arrays.asList(
+            buildFrame(
+                "java.net.PlainSocketImpl", "socketAccept", "(Ljava/net/SocketImpl;)V", -1, 0),
+            buildFrame(
+                "java.net.AbstractPlainSocketImpl", "accept", "(Ljava/net/SocketImpl;)V", 458, 7),
+            buildFrame("java.net.ServerSocket", "implAccept", "(Ljava/net/Socket;)V", 551, 60),
+            buildFrame("java.net.ServerSocket", "accept", "()Ljava/net/Socket;", 519, 48),
+            buildFrame(
+                "sun.rmi.transport.tcp.TCPTransport$AcceptLoop",
+                "executeAcceptLoop",
+                "()V",
+                394,
+                42),
+            buildFrame("sun.rmi.transport.tcp.TCPTransport$AcceptLoop", "run", "()V", 366, 1),
+            buildFrame("java.lang.Thread", "run", "()V", 834, 11));
+    when(mockStackTrace.getFrames()).thenReturn(frames);
   }
 
   @Test
@@ -66,27 +82,23 @@ class ProfileSummarizerTest {
         .thenAnswer(invocation -> invocation.getArgument(0, String.class));
     ProfileSummarizer testClass = ProfileSummarizer.forExecutionSample(nameNormalizer);
 
-    try (MockedStatic<MethodSupport> methodSupport = Mockito.mockStatic(MethodSupport.class)) {
-      methodSupport.when(() -> MethodSupport.serialize(any())).thenReturn(stackTrace);
+    try (MockedStatic<RecordedObjectValidators> recordedObjectValidator =
+        Mockito.mockStatic(RecordedObjectValidators.class)) {
+      recordedObjectValidator
+          .when(() -> RecordedObjectValidators.hasField(any(), any(), any()))
+          .thenReturn(true);
 
-      try (MockedStatic<RecordedObjectValidators> recordedObjectValidator =
-          Mockito.mockStatic(RecordedObjectValidators.class)) {
-        recordedObjectValidator
-            .when(() -> RecordedObjectValidators.hasField(any(), any(), any()))
-            .thenReturn(true);
-
-        testClass.accept(mockEvent);
-        testClass.accept(mockEvent);
-        testClass.accept(mockEvent2);
-        testClass.accept(mockEvent2);
-      }
-
-      Map<String, List<JvmStackTraceEvent>> result = testClass.getStackTraceEventPerThread();
-
-      assertEquals(2, result.size());
-      assertEquals(2, result.get("thread-1").size());
-      assertEquals(2, result.get("thread-2").size());
+      testClass.accept(mockEvent);
+      testClass.accept(mockEvent);
+      testClass.accept(mockEvent2);
+      testClass.accept(mockEvent2);
     }
+
+    Map<String, List<JvmStackTraceEvent>> result = testClass.getStackTraceEventPerThread();
+
+    assertEquals(2, result.size());
+    assertEquals(2, result.get("thread-1").size());
+    assertEquals(2, result.get("thread-2").size());
   }
 
   @Test
@@ -95,39 +107,35 @@ class ProfileSummarizerTest {
         .thenAnswer(invocation -> invocation.getArgument(0, String.class));
     ProfileSummarizer testClass = ProfileSummarizer.forExecutionSample(nameNormalizer);
 
-    try (MockedStatic<MethodSupport> methodSupport = Mockito.mockStatic(MethodSupport.class)) {
-      methodSupport.when(() -> MethodSupport.serialize(any())).thenReturn(stackTrace);
-
-      try (MockedStatic<RecordedObjectValidators> recordedObjectValidator =
-          Mockito.mockStatic(RecordedObjectValidators.class)) {
-        recordedObjectValidator
-            .when(() -> RecordedObjectValidators.hasField(any(), any(), any()))
-            .thenReturn(true);
-        testClass.accept(mockEvent);
-        testClass.accept(mockEvent);
-        testClass.accept(mockEvent2);
-        testClass.accept(mockEvent2);
-      }
-
-      List<Event> resultEvents = testClass.summarize().collect(Collectors.toList());
-
-      assertEquals(16, resultEvents.size());
-      assertEquals(
-          8,
-          (int)
-              resultEvents
-                  .stream()
-                  .filter(
-                      e -> e.getAttributes().asMap().get(THREAD_NAME).toString().equals("thread-1"))
-                  .count());
-      assertEquals(
-          16,
-          resultEvents
-              .stream()
-              .filter(e -> e.getAttributes().asMap().get(THREAD_NAME).toString().equals("thread-1"))
-              .mapToInt(e -> (int) e.getAttributes().asMap().get(FLAME_VALUE))
-              .sum());
+    try (MockedStatic<RecordedObjectValidators> recordedObjectValidator =
+        Mockito.mockStatic(RecordedObjectValidators.class)) {
+      recordedObjectValidator
+          .when(() -> RecordedObjectValidators.hasField(any(), any(), any()))
+          .thenReturn(true);
+      testClass.accept(mockEvent);
+      testClass.accept(mockEvent);
+      testClass.accept(mockEvent2);
+      testClass.accept(mockEvent2);
     }
+
+    List<Event> resultEvents = testClass.summarize().collect(Collectors.toList());
+
+    assertEquals(16, resultEvents.size());
+    assertEquals(
+        8,
+        (int)
+            resultEvents
+                .stream()
+                .filter(
+                    e -> e.getAttributes().asMap().get(THREAD_NAME).toString().equals("thread-1"))
+                .count());
+    assertEquals(
+        16,
+        resultEvents
+            .stream()
+            .filter(e -> e.getAttributes().asMap().get(THREAD_NAME).toString().equals("thread-1"))
+            .mapToInt(e -> (int) e.getAttributes().asMap().get(FLAME_VALUE))
+            .sum());
   }
 
   @Test
@@ -135,19 +143,15 @@ class ProfileSummarizerTest {
     when(nameNormalizer.getNormalizedThreadName(any(String.class))).thenReturn("thread-#");
     ProfileSummarizer testClass = ProfileSummarizer.forExecutionSample(nameNormalizer);
 
-    try (MockedStatic<MethodSupport> methodSupport = Mockito.mockStatic(MethodSupport.class)) {
-      methodSupport.when(() -> MethodSupport.serialize(any())).thenReturn(stackTrace);
-
-      try (MockedStatic<RecordedObjectValidators> recordedObjectValidator =
-          Mockito.mockStatic(RecordedObjectValidators.class)) {
-        recordedObjectValidator
-            .when(() -> RecordedObjectValidators.hasField(any(), any(), any()))
-            .thenReturn(true);
-        testClass.accept(mockEvent);
-        testClass.accept(mockEvent);
-        testClass.accept(mockEvent2);
-        testClass.accept(mockEvent2);
-      }
+    try (MockedStatic<RecordedObjectValidators> recordedObjectValidator =
+        Mockito.mockStatic(RecordedObjectValidators.class)) {
+      recordedObjectValidator
+          .when(() -> RecordedObjectValidators.hasField(any(), any(), any()))
+          .thenReturn(true);
+      testClass.accept(mockEvent);
+      testClass.accept(mockEvent);
+      testClass.accept(mockEvent2);
+      testClass.accept(mockEvent2);
 
       List<Event> resultEvents = testClass.summarize().collect(Collectors.toList());
 
@@ -176,22 +180,29 @@ class ProfileSummarizerTest {
         .thenAnswer(invocation -> invocation.getArgument(0, String.class));
     ProfileSummarizer testClass = ProfileSummarizer.forExecutionSample(nameNormalizer);
 
-    try (MockedStatic<MethodSupport> methodSupport = Mockito.mockStatic(MethodSupport.class)) {
-      methodSupport.when(() -> MethodSupport.serialize(any())).thenReturn(stackTrace);
-
-      try (MockedStatic<RecordedObjectValidators> recordedObjectValidator =
-          Mockito.mockStatic(RecordedObjectValidators.class)) {
-        recordedObjectValidator
-            .when(() -> RecordedObjectValidators.hasField(any(), any(), any()))
-            .thenReturn(true);
-        testClass.accept(mockEvent2);
-        testClass.accept(mockEvent);
-      }
-
-      List<Event> resultEvents = testClass.summarize().collect(Collectors.toList());
-
-      assertEquals(16, resultEvents.size());
-      assertEquals(16, resultEvents.stream().filter(e -> e.getTimestamp() == 1L).count());
+    try (MockedStatic<RecordedObjectValidators> recordedObjectValidator =
+        Mockito.mockStatic(RecordedObjectValidators.class)) {
+      recordedObjectValidator
+          .when(() -> RecordedObjectValidators.hasField(any(), any(), any()))
+          .thenReturn(true);
+      testClass.accept(mockEvent2);
+      testClass.accept(mockEvent);
     }
+
+    List<Event> resultEvents = testClass.summarize().collect(Collectors.toList());
+
+    assertEquals(16, resultEvents.size());
+    assertEquals(16, resultEvents.stream().filter(e -> e.getTimestamp() == 1L).count());
+  }
+
+  private RecordedFrame buildFrame(
+      String className, String methodName, String descriptor, int line, int bytecodeIndex) {
+    RecordedFrame frame = mock(RecordedFrame.class, Answers.RETURNS_DEEP_STUBS);
+    when(frame.getMethod().getType().getName()).thenReturn(className);
+    when(frame.getMethod().getName()).thenReturn(methodName);
+    when(frame.getMethod().getDescriptor()).thenReturn(descriptor);
+    when(frame.getLineNumber()).thenReturn(line);
+    when(frame.getBytecodeIndex()).thenReturn(bytecodeIndex);
+    return frame;
   }
 }
